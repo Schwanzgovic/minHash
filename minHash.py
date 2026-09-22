ee40333b518a091da960a6403d9911d6e41ae689
matildact <matildact@gmail.com>
2026-09-22 11:31:00 +0200

    Update minHash.py
    new way of reading fasta file, not a function but to be used in one.
    commented out code that we don't need.
    replaced the kmers function with one that directly transforms them into integers (mapping).

--- minHash.py
+++ minHash.py
@@ -1,41 +1,67 @@
+import hashlib
+from Bio import SeqIO
 
 # function to read the genome files and return a list of strings (xxx note: maybe we want it as a dictionary, thinking about problem 2)
 # each string should represent one genome 
 
-def readFileAndReturnStringList(filename):
-    listOfStrings = []
-    string = ""
-    # TODO
-    return listOfStrings
+#def readFileAndReturnStringList(filename):
+#    listOfStrings = []
+#    string = ""
+#	with open(filename, 'r') as file:
+#		for line in file:
+#			if(line[0]=='>' and string != ''):
+#				listOfStrings.append(string)
+#				string=""
+#			else:
+#				if(line[0]!='>'):
+#					string += line.strip()
+#	listOfStrings.append(string)
+#	return listOfStrings
+
+# this reads a chosen fasta file, we add it to our final function
+genomes = {rec.id: str(rec.seq) for rec in SeqIO.parse(file_fasta, "fasta")}
+
+
 
 # this function takes a string and returns all subsequences of this string with length k
 
-def kMers(string, k): 
-    listOfKmers = []
-    for i in range(len(string)-k+1):
-        listOfKmers.append(string[i:i+k])
-    # TODO : gives all k-mers, maybe add a comparison so that identical k-mers are ignored
-    return listOfKmers
+#def kMers(string, k): 
+ #   listOfKmers = []
+  #  for i in range(len(string)-k+1):
+   #     listOfKmers.append(string[i:i+k])
+   # return listOfKmers
 
+#takes a sequence as string and returns all kmers as intergers in a list (xxx i am using zlib here!)
+def get_kmer_ints(sequence,k):
+    kmer_ints =[]
+    
+    #loop through secuenqe and make all kmers
+    for i in range(len(sequence) - k +1):
+        kmer = sequence[i : i+k]
+        
+        # converts the string to a unique 32bit interger
+        kmer_int = zlib.crc32(kmer.encode('utf-8')) 
+        
+        kmer_ints.append(kmer_int)
+    return kmer_ints
 
+def
 # function to get m deterministic hash functions with set seeds
 def get_hash_functions(m):
     # TODO
     return #list of hash functions
 
 # function to compute the min hash for a given hash function and given set of k_mers
-
-def minHashValue(kMers, randomHashfunction):
+#def minHashValue(kMers, randomHashfunction):
     # initialize old value with the hash value of the first k-mer
-    minHash = randomHashfunction(kMers[0])
-    for kMer in kMers:
-        newValue = randomHashfunction(kMer)
-        if minHash > newValue: # xxx i think it should be < but not sure so i keep it as a comment
-            continue
-        else: 
-            minHash = newValue
-
-    return minHash
+#    minHash = randomHashfunction(kMers[0])
+ #   for kMer in kMers:
+  #      newValue = randomHashfunction(kMer)
+   #     if minHash > newValue: # xxx i think it should be < but not sure so i keep it as a comment
+    #        continue
+     #   else: 
+      #      minHash = newValue
+   # return minHash
 
 # this function creates a sketch (xxx like shown in the lecture : streaming) from a list of kmers and a list of m different hash functions
 def create_sketch(kMers, hash_functions)
@@ -63,6 +89,9 @@ def estimate_jaccard(sketch_A, sketch_B):
 
 
 # xxx i dont know if we want to do the gonomic distance as a seperate function or combine it with the jaccard
+
+# this reads a chosen fasta file, we add it to our final function
+# genomes = {rec.id: str(rec.seq) for rec in SeqIO.parse(file_fasta, "fasta")}
 
 
 
