89acd7264681b3387ac06dde988ff406ef8b73f8
uiteg <uiteg@student.kit.edu>
2026-09-23 17:27:06 +0200

    tried to create the evolutionaryTree the estimateParentDistances and updateDistanceMatrix function need to be updated

--- evolutionaryTreeNikolai.py
+++ evolutionaryTreeNikolai.py
@@ -87,10 +87,102 @@ def computeAverageDistances(dist_matrix):
                 sum += dist_matrix[i][j]
         averageDistances[i] = sum/(n-1)
     return averageDistances
-    
+
+# replaces every entry on and below the diagonal with a two
+# this code assumes an n x n matrix (square)
+def lowerTriangleTwos(matrix):
+    lowerTriangularTwoMatrix = matrix
+    n = matrix.shape[0]
+    for i in range(n):
+        for j in range(i+1):
+           matrix[i][j] = 2 
+    return np.array(lowerTriangularTwoMatrix)
+
+# finds the indices of a matrix where the matrix has a minimal value
+def findMinimumIndicesOfMatrix(matrix):
+    n = matrix.shape[0]
+    oldMin = matrix[0][np.argmin(matrix[0])]
+    columnIndex = np.argmin(matrix[0])
+    rowIndex = 0
+    for i in range(n):
+        newMinIndex = np.argmin(matrix[i])
+        newMin = matrix[i][newMinIndex]
+        if newMin < oldMin:
+            columnIndex = newMinIndex
+            rowIndex = i
+    return rowIndex, columnIndex
+
+# estimates the distances from the parent node to all other nodes by taking the average distances of its children
+
+def estimateParentDistances(distanceMatrix, minIndexA, minIndexB):
+    n = distanceMatrix.shape[0]
+    parentDistances = np.ones(n-1)
+    minAB = min(minIndexA, minIndexB)
+    maxAB = max(minIndexA, minIndexB)
+    parentDistances[minAB] = 0
+    for k in range(n):
+        if k == maxAB:
+            continue
+        elif k != minAB:
+            parentDistances[k] = (distanceMatrix[k][minIndexA] + distanceMatrix[minIndexB])/2
+        
+    return parentDistances        
+
+def updateDistanceMatrix(distanceMatrix, minIndexA, minIndexB):
+    n = distanceMatrix.shape[0]
+    updateMatrix = np.ones((n-1, n-1))
+    minAB = min(minIndexA, minIndexB)
+    maxAB = max(minIndexA, minIndexB)
+    # TODO
+    for i in range(n-1):
+        for j in range(n-1):
+            if i < minAB and j < minAB:
+                updateMatrix[i][j] = distanceMatrix[i][j]
+
+            elif i != minAB and j == minAB:
+                # for the first of the two indices, put the parent node (or ancestor here)
+                # we estimate it's distances to all other nodes by taking the average of the children
+                updateMatrix[i][j] = (distanceMatrix[i][j] + distanceMatrix[i][maxAB])/2
+            elif i == minAB and j != minAB:
+                updateMatrix[i][j] = 1
+            elif i < maxAB and j < maxAB:
+                # when both indices are between the minIndexA and minIndexB just copy the entries
+                updateMatrix[i][j] = distanceMatrix[i][j]
+
+            elif i < maxAB and j >= maxAB:
+                # skip the maxAB column
+                updateMatrix[i][j] = distanceMatrix[i][j+1]
+            elif i >= maxAB and j < maxAB:
+                # skip the maxAB row
+                updateMatrix[i][j] = distanceMatrix[i+1][j]
+            else:
+                # skip the maxAB row and maxAB column
+                updateMatrix[i][j] = distanceMatrix[i+1][j+1]
+
+    return updateMatrix
+
+ 
+# function to build the evolutionary tree. It returns an array which satisfies if paranet is at array[n]
+# then array[2n+1] and array[2n+2] are its children
+def constructEvolutionaryTree(distance_matrix, genome_IDS):
+    n = len(genome_IDS)
+    # returns the upper triangle of matrix and cuts out the zeros (also cuts out the doubled distances)
+    updatedMatrix = lowerTriangleTwos(distance_matrix)
+    evolutionaryTree = []
+    # the loop has to run exactly n-2 times
+    for k in range(n-1): 
+        minIndexA, minIndexB = findMinimumIndicesOfMatrix(updatedMatrix)
+        updatedMatrix = updateDistanceMatrix(updatedMatrix, minIndexA, minIndexB)
+
+        parentNode = chr(65 + k)
+        childOne = genome_IDS(minIndexA)
+        childTwo = genome_IDS(minIndexB)
+        evolutionaryTree.append([parentNode, childOne, childTwo])
+
+    return evolutionaryTree
 #Solves problem 1    
 # 1. read all genomes from FASTA-file
-genomes = {rec.id: str(rec.seq) for rec in SeqIO.parse("test3.fa", "fasta")} #change "test3.fa" to wanted file
+genomes = {rec.id: str(rec.seq) for rec in SeqIO.parse("test1.fa", "fasta")} #change "test3.fa" to wanted file
 
 # 2. choose parameters
 k = 21  # standard for bacteria (i googled, but we might want to play around with it and find a justification)
@@ -108,8 +200,14 @@ print(f"Created {len(sketches)} sketches") # xxx just to check we can remove lat
 dist_matrix, genome_ids = create_distance_martix(sketches) 
 
 print("Distance matrix: ", "\n", dist_matrix,"\n", "Genomde IDs: ", "\n", genome_ids)
+triangularMatrix = lowerTriangleTwos(dist_matrix)
+minIndexRow, minIndexColumn = findMinimumIndicesOfMatrix(triangularMatrix)
 
+updateMatrix = updateDistanceMatrix(dist_matrix, minIndexRow, minIndexColumn)
 averageDistancesToAllOtherNodes = computeAverageDistances(dist_matrix)
-print("average Distances: ", averageDistancesToAllOtherNodes)
-print("Found root: ", findRoot(averageDistancesToAllOtherNodes, genome_ids))
-
+parentDistances = estimateParentDistances(dist_matrix, minIndexRow, minIndexColumn)
+#print("average Distances: ", averageDistancesToAllOtherNodes)
+print("Upper traingle of matrix: ", "\n", triangularMatrix)
+print("Min Index of upper triangle: ", minIndexRow, " ", minIndexColumn)
+print("Distances for the parent node: ", "\n", parentDistances)
+#print("And finally the updated Matrix: ", "\n", updateMatrix)
